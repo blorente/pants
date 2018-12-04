cf1fd9cd584e78a1b3289fb69fc087d48f79b646
Andreea Dimitriu <adimitriu@twitter.com>
2018-12-04 12:32:58 +0000

    Working script

--- testprojects/cooldeps/process_graph.py
+++ testprojects/cooldeps/process_graph.py
@@ -5,23 +5,28 @@ from pprint import pprint
 
 json_file = sys.argv[1]
 
-path_to_buildozer = "./pants-support/buildifier/bin/buildozer"
+path_to_buildozer = "pants-support/buildifier/bin/buildozer"
 
 def build_command(action, dependency):
-  cmd = "'" + action + " dependencies " + dependency + "'"
-  print("command = ", cmd, "\n")
-  return cmd
+  parts = dependency.split(":")
+  if parts[0].endswith(parts[1]):
+    dependency = parts[0]
+  return ' "' + action + ' dependencies ' + dependency + '" '
 
 def add_dependency(toAdd, target):
   print("ADD: toAdd = ", toAdd, "to target = ", target, "\n")
   cmd = build_command("add", toAdd)
-  subprocess.run([path_to_buildozer, cmd, target])
+  full_cmd = " ".join([path_to_buildozer, cmd, target])
+  print ("full_cmd = ", full_cmd, "\n")
+  subprocess.run(full_cmd, shell=True)
 
 
 def remove_dependency(toRemove, target):
   print("DEL: toRemove = ", toRemove, " from target = ", target, "\n")
   cmd = build_command("remove", toRemove)
-  subprocess.run([path_to_buildozer, cmd, target])
+  full_cmd = " ".join([path_to_buildozer, cmd, target])
+  print ("full_cmd = ", full_cmd, "\n")
+  subprocess.run(full_cmd, shell=True)
 
 with open(json_file) as f:
   data = json.load(f)
