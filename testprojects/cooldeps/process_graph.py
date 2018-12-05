263d7d83e9d23b53612952540a57d6491c4bcf95
Andreea Dimitriu <adimitriu@twitter.com>
2018-12-05 16:36:42 +0000

    Refactored script

--- testprojects/cooldeps/process_graph.py
+++ testprojects/cooldeps/process_graph.py
@@ -3,34 +3,41 @@ import subprocess
 import sys
 from pprint import pprint
 
-json_file = sys.argv[1]
-
 path_to_buildozer = "pants-support/buildifier/bin/buildozer"
+add_action = "add dependencies "
+remove_action = "remove dependencies "
+strict_deps_action = "print strict deps"
+
+# read the dep-usage graph
+universe = sys.argv[1]
+
+# read the direct dependees
+direct_dependees = sys.argv[2]
 
 def build_command(action, dependency):
+  # buildozer doesn't recognize a pattern of type "a/target/T:T"
+  # in which case the last part needs to be removed
   parts = dependency.split(":")
   if parts[0].endswith(parts[1]):
     dependency = parts[0]
-  return ' "' + action + ' dependencies ' + dependency + '" '
+  return ' "' + action + dependency + '" '
 
-def add_dependency(toAdd, target):
+def call_buildozer(action, dependency, target):
+  cmd = build_command(action, dependency)
+  full_cmd = " ".join([path_to_buildozer, cmd, target])
+  subprocess.run(full_cmd, shell=True)
+
+def add_dependency(dependency, target):
   print("ADD: toAdd = ", toAdd, "to target = ", target, "\n")
-  cmd = build_command("add", toAdd)
-  full_cmd = " ".join([path_to_buildozer, cmd, target])
-  print ("full_cmd = ", full_cmd, "\n")
-  subprocess.run(full_cmd, shell=True)
+  call_buildozer(add_action, dependency, target)
 
 
-def remove_dependency(toRemove, target):
+def remove_dependency(dependency, target):
   print("DEL: toRemove = ", toRemove, " from target = ", target, "\n")
-  cmd = build_command("remove", toRemove)
-  full_cmd = " ".join([path_to_buildozer, cmd, target])
-  print ("full_cmd = ", full_cmd, "\n")
-  subprocess.run(full_cmd, shell=True)
+  call_buildozer(remove_action, dependency, target)
 
-with open(json_file) as f:
+with open(universe) as f:
   data = json.load(f)
-  #print(data)
 
   targets_and_dependencies = {}
   for target, payload in data.items():
