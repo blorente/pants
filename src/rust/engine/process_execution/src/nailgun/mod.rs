4f2d33e27d6a304e7990e72be6102aec86c534e7
Borja Lorente <bescobar@twitter.com>
2019-10-03 11:05:28 +0100

    WIP

--- src/rust/engine/process_execution/src/nailgun/mod.rs
+++ src/rust/engine/process_execution/src/nailgun/mod.rs
@@ -1,10 +1,11 @@
 
 use super::CommandRunner;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use crate::{MultiPlatformExecuteProcessRequest, FallibleExecuteProcessResult, ExecuteProcessRequest};
 use workunit_store::WorkUnitStore;
 use futures::Future;
 use boxfuture::BoxFuture;
+use std::path::PathBuf;
 
 pub mod nailgun_process_map;
 
@@ -12,7 +13,7 @@ pub type NailgunProcessMap = nailgun_process_map::NailgunProcessMap;
 
 struct NailgunCommandRunner {
     inner: Arc<dyn CommandRunner>,
-    nailguns: NailgunProcessMap
+    nailguns: NailgunProcessMap,
 }
 
 impl NailgunCommandRunner {
@@ -24,12 +25,35 @@ impl NailgunCommandRunner {
     }
 }
 
+fn is_client_arg(arg: &String) -> bool {
+    arg.starts_with("@")
+}
+
 impl super::CommandRunner for NailgunCommandRunner {
-    fn run(&self, req: MultiPlatformExecuteProcessRequest, workunit_store: WorkUnitStore) -> BoxFuture<FallibleExecuteProcessResult, String> {
-        unimplemented!()
+    fn run(
+        &self,
+        req: MultiPlatformExecuteProcessRequest,
+        workunit_store: WorkUnitStore) -> BoxFuture<FallibleExecuteProcessResult, String> {
+
+        // HACK Transform the nailgun req into nailgun startup args by heuristically transforming the request
+        let mut client_req = self.extract_compatible_request(&req).unwrap();
+        let mut nailgun_req = self.extract_compatible_request(&req).unwrap();
+
+        nailgun_req.argv.retain(|arg| !is_client_arg(arg));
+
+        let nailgun_name = nailgun_req.argv.last().unwrap().clone(); // We assume the last one is the main class name
+        self.nailguns.connect(nailgun_name, nailgun_req).and_then(|res| {
+            let metadata = self.nailguns.get(&nailgun_name).unwrap();
+            client_req.argv.retain(is_client_arg);
+            client_req.jdk_home = Some(PathBuf::from("~/workspace/bescobar/otherpants/mock_jdk".into()));
+            client_req.env.insert("NAILGUN_PORT".into(), metadata.port.to_string());
+
+            println!("Running Client EPR {:#?} on Nailgun", client_req);
+            self.inner.run(MultiPlatformExecuteProcessRequest::from(client_req), workunit_store)
+        })
     }
 
     fn extract_compatible_request(&self, req: &MultiPlatformExecuteProcessRequest) -> Option<ExecuteProcessRequest> {
-        unimplemented!()
+        self.inner.extract_compatible_request(req)
     }
 }
